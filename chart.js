d6656044570bf212fb696051aa2dac723b6a82ec
Divya-Ruth-Bennett <111144958+Divya-Ruth-Bennett@users.noreply.github.com>
2025-04-25 17:02:20 -0400

    Create chart.js

---
+++ chart.js
@@ -0,0 +1,34 @@
+{
+  "name": "Grouped + Stacked Bar Chart",
+  "description": "A custom chart combining grouped and stacked bars using Chart.js",
+  "author": "Your Name",
+  "homepage": "https://your-github-username.github.io/custom-bar-chart/",
+  "visualization": {
+    "script": "chart.js",
+    "style": "index.html"
+  },
+  "data": {
+    "requiredFields": [
+      {
+        "id": "dimension",
+        "name": "Dimension",
+        "type": "DIMENSION"
+      },
+      {
+        "id": "metric1",
+        "name": "Metric 1",
+        "type": "METRIC"
+      },
+      {
+        "id": "metric2",
+        "name": "Metric 2",
+        "type": "METRIC"
+      },
+      {
+        "id": "metric3",
+        "name": "Metric 3",
+        "type": "METRIC"
+      }
+    ]
+  }
+}
